ab7958bdf59fc960141bf81c038680d9c7072af2
sekanson <94586021+sekanson@users.noreply.github.com>
2022-07-05 20:35:40 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <iframe width="768" height="432" src="https://miro.com/app/live-embed/o9J_lj9dcu4=/?moveToViewport=-1570,-2255,2614,2437&embedId=707277321648" frameborder="0" scrolling="no" allowfullscreen></iframe>
+        <iframe width="1012" height="1700" src="https://miro.com/app/live-embed/o9J_lj9dcu4=/?moveToViewport=-1570,-2255,2614,2437&embedId=415936286538" frameborder="0" scrolling="no" allowfullscreen></iframe>
         <Header title="Welcome to my app!" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>

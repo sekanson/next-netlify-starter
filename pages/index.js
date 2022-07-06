678abe6a8c5ce8bdb54f049087cf2cc0b4c9f02e
sekanson <94586021+sekanson@users.noreply.github.com>
2022-07-05 20:41:31 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,11 +11,8 @@ export default function Home() {
       </Head>
 
       <main>
-        <iframe width="1012" height="1700" src="https://miro.com/app/live-embed/o9J_lj9dcu4=/?moveToViewport=-1570,-2255,2614,2437&embedId=415936286538" frameborder="0" scrolling="no" allowfullscreen></iframe>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+       <iframe width="1012" height="1400" src="https://miro.com/app/live-embed/o9J_lj9dcu4=/?moveToViewport=-1570,-2255,2614,2437&embedId=653769344612" frameborder="0" scrolling="no" allowfullscreen></iframe>
+       <Header title="Ford x xix3D work flow" />
       </main>
 
       <Footer />

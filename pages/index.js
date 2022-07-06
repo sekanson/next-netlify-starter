c31ed3714748a3105e725a090cc8c7a05fc60f47
sekanson <94586021+sekanson@users.noreply.github.com>
2022-07-05 20:46:37 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,6 +13,9 @@ export default function Home() {
       <main>
        <iframe width="1012" height="1400" src="https://miro.com/app/live-embed/o9J_lj9dcu4=/?moveToViewport=-1570,-2255,2614,2437&embedId=653769344612" frameborder="0" scrolling="no" allowfullscreen></iframe>
        <Header title="Ford x xix3D work flow" />
+       <p className="description">
+           
+        </p>
       </main>
 
       <Footer />

29053f496582ad7f41352fcee26e9415db7d169c
sekanson <94586021+sekanson@users.noreply.github.com>
2022-07-05 20:23:05 -0400

    Update basic.cy.js

--- cypress/e2e/basic.cy.js
+++ cypress/e2e/basic.cy.js
@@ -7,6 +7,7 @@ describe('sample test', () => {
     cy.get('h1')
     .contains('Welcome to my app!');
   })
+  <iframe width="768" height="432" src="https://miro.com/app/live-embed/o9J_lj9dcu4=/?moveToViewport=-1570,-2255,2614,2437&embedId=707277321648" frameborder="0" scrolling="no" allowfullscreen></iframe>
   it('renders the Netlify logo image', () => {
     cy.get('img')
     .should('be.visible')
